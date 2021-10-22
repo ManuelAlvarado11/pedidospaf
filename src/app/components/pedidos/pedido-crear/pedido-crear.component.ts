b5999f1cd25326993abe1f08356bdab6bddf528d
ManuelAlvarado11 <av16002@ues.edu.sv>
2021-10-22 10:59:42 -0600

    Configurando Paquete Toastr (Notificacions)

--- src/app/components/pedidos/pedido-crear/pedido-crear.component.ts
+++ src/app/components/pedidos/pedido-crear/pedido-crear.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,FormArray,FormControl,Validators } from '@angular/forms';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Pedido } from 'src/app/models/pedido';
+import { PedidoService } from 'src/app/services/pedido.service';
 
 @Component({
   selector: 'app-pedido-crear',
@@ -9,7 +12,9 @@ import { FormGroup,FormBuilder,FormArray,FormControl,Validators } from '@angular
 export class PedidoCrearComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, 
+              private pedidoService: PedidoService,
+              private toastr: ToastrService) { 
     this.form = this.formBuilder.group({
       cot_empresa: ['',[Validators.required,Validators.maxLength(6)]],
       cot_numero: ['',[Validators.required,Validators.maxLength(10)]],
@@ -25,6 +30,19 @@ export class PedidoCrearComponent implements OnInit {
   }
 
   guardarPedido(){
-    console.log(this.form);
+    const pedido: Pedido = {
+      cot_empresa: this.form.get('cot_empresa')!.value,
+      cot_numero: this.form.get('cot_numero')!.value,
+      cot_pedido: this.form.get('cot_pedido')!.value,
+      cot_vendedor: this.form.get('cot_vendedor')!.value,
+      cot_bodega: this.form.get('cot_bodega')!.value,
+      cot_cliente: this.form.get('cot_cliente')!.value,
+      cot_total: this.form.get('cot_total')!.value,
+    }
+
+    this.pedidoService.guardarPedido(pedido).subscribe(data => {
+      this.toastr.success('Registro Agregado', 'Pedido Agregado Exitosamente');
+      this.form.reset();
+    });
   }
 }
--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pedido } from '../models/pedido';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidoService {
+  myAppUrl = 'http://localhost:60215/';
+  myApiUrl = 'api/fac_pedidos/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  guardarPedido(pedido: Pedido):Observable<Pedido>{
+    return this.http.post<Pedido>(this.myAppUrl + this.myApiUrl, pedido);
+  }
+
 }

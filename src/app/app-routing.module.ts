d30ada46431124db91b0de2edabccb18013538f8
ManuelAlvarado11 <av16002@ues.edu.sv>
2021-10-26 16:06:28 -0600

    Ajustes en Table Principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'sidebar', component: SidebarComponent},
+  {path:'navbar', component: NavbarComponent},
+  {path:'pedidos', component: PedidosComponent},
+  {path:'footer', component: FooterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

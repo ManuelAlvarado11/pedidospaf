31e54a26cae19e479b9ba39fabea8469aaacaa2a
ManuelAlvarado11 <av16002@ues.edu.sv>
2021-11-12 14:36:31 -0600

    CalcularTotales con ngModel standalone true

--- src/app/components/pedidos/pedido-crear/pedido-crear.component.ts
+++ src/app/components/pedidos/pedido-crear/pedido-crear.component.ts
@@ -1,6 +1,6 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
 import { DetallePedido } from 'src/app/models/detallePedido';
@@ -17,39 +17,38 @@ import { VendedorService } from 'src/app/services/vendedor.service';
   styleUrls: ['./pedido-crear.component.css']
 })
 export class PedidoCrearComponent implements OnInit, OnDestroy {
-  form: FormGroup;
-  formDetalle: FormGroup;
+  //Variables
+  formPedido: FormGroup;
   subscription!: Subscription;
   pedido!: Pedido;
   detalle_pedidos!: DetallePedido[];
+  total_pedido = 0;
   cot_empresa = "";cot_numero = "";cot_pedido="";
   userSesion = JSON.parse(localStorage.getItem('usuario')!);
 
+  //Constructor
   constructor(private formBuilder: FormBuilder, 
               private toastr: ToastrService,
+              private route: Router,
               private pedidoService: PedidoService,
               public bodegaService:BodegaService,
               public tipoDocumentoService:TipoDocumentoService,
               public vendedorService:VendedorService) { 
-    
-    this.form = this.formBuilder.group({
+    //Declaracion de FormPedido
+    this.formPedido = this.formBuilder.group({
       cot_numero: ['',[Validators.required,Validators.maxLength(25)]],
       cot_pedido: ['',[Validators.required,Validators.maxLength(25)]],
       cot_vendedor: ['',[Validators.required,Validators.maxLength(25)]],
       cot_bodega: ['',[Validators.required,Validators.maxLength(25)]],
       cot_cliente: ['',[Validators.required,Validators.maxLength(25)]],
       cot_tipo_documento: ['',[Validators.required,Validators.maxLength(25)]],
-      cot_total: ['',[Validators.required,Validators.max(10000)]]   
+      formDetalle: this.formBuilder.group({
+        dct_numero_detalle: ['',[Validators.required,Validators.maxLength(25)]],
+        dct_producto: ['',[Validators.required,Validators.maxLength(25)]],
+        dct_cantidad: ['',[Validators.required,Validators.max(10000)]],
+        dct_precio_descuento: ['',[Validators.required,Validators.max(10000)]]
+      })
     });
-
-    this.formDetalle = this.formBuilder.group({
-      dct_numero_detalle: ['',[Validators.required,Validators.maxLength(25)]],
-      dct_producto: ['',[Validators.required,Validators.maxLength(25)]],
-      dct_cantidad: ['',[Validators.required,Validators.max(10000)]],
-      dct_precio_descuento: ['',[Validators.required,Validators.max(10000)]],
-      dct_total: ['',[Validators.required,Validators.max(10000)]]
-    });
-    
   }
 
   ngOnInit(): void {
@@ -57,14 +56,13 @@ export class PedidoCrearComponent implements OnInit, OnDestroy {
     this.pedidoService.obtenerPedido().subscribe(data => { 
       this.pedido =data;
       this.detalle_pedidos = [];
-      this.form.patchValue({
+      this.formPedido.patchValue({
         cot_numero: this.pedido.cot_numero,
         cot_pedido: this.pedido.cot_pedido,
         cot_bodega: this.pedido.cot_bodega,
         cot_vendedor: this.pedido.cot_vendedor,
         cot_cliente: this.pedido.cot_cliente,
-        cot_tipo_documento: this.pedido.cot_tipo_documento,
-        cot_total: this.pedido.cot_total
+        cot_tipo_documento: this.pedido.cot_tipo_documento
       });
       this.cot_empresa = this.pedido.cot_empresa;
       this.cot_numero = this.pedido.cot_numero;
@@ -73,14 +71,14 @@ export class PedidoCrearComponent implements OnInit, OnDestroy {
     
     //CARGAR CLIENTE
     this.pedidoService.obtenerCliente().subscribe(data => {
-      this.form.patchValue({
+      this.formPedido.patchValue({
         cot_cliente: data.cli_codigo
       });
     });
 
     //CARGAR PRODUCTO
     this.pedidoService.obtenerProducto().subscribe(data => {
-      this.formDetalle.patchValue({
+      this.formPedido.get('formDetalle')!.patchValue({
         dct_producto: data.pro_codigo
       });
     });
@@ -114,62 +112,85 @@ export class PedidoCrearComponent implements OnInit, OnDestroy {
   agregar(){
     const pedido: Pedido = {
       cot_empresa: this.userSesion.empresa,
-      cot_numero: this.form.get('cot_numero')!.value,
-      cot_pedido: this.form.get('cot_pedido')!.value,
-      cot_vendedor: this.form.get('cot_vendedor')!.value,
-      cot_bodega: this.form.get('cot_bodega')!.value,
-      cot_cliente: this.form.get('cot_cliente')!.value,
-      cot_tipo_documento: this.form.get('cot_tipo_documento')!.value,
-      cot_total: this.form.get('cot_total')!.value,
+      cot_numero: this.formPedido.get('cot_numero')!.value,
+      cot_pedido: this.formPedido.get('cot_pedido')!.value,
+      cot_vendedor: this.formPedido.get('cot_vendedor')!.value,
+      cot_bodega: this.formPedido.get('cot_bodega')!.value,
+      cot_cliente: this.formPedido.get('cot_cliente')!.value,
+      cot_tipo_documento: this.formPedido.get('cot_tipo_documento')!.value,
+      cot_total: this.total_pedido,
       cot_anulada: false,
       detalles: this.detalle_pedidos
     }
-
+    console.log(pedido);
     this.pedidoService.guardarPedido(pedido).subscribe(data => {
       this.toastr.success('Registro Agregado', 'Pedido Agregado Exitosamente');
       this.pedidoService.obtenerPedidos();
-      this.form.reset();
-      this.formDetalle.reset();
+      this.formPedido.reset();
+      this.route.navigate(['/home/pedidos/listar']);
     });
   }
 
   agregarDetalle(){
     const detalle_pedido: DetallePedido={
       dct_empresa: this.userSesion.empresa,
-      dct_cotizacion: this.form.get('cot_numero')!.value,
-      dct_numero_detalle: this.formDetalle.get('dct_numero_detalle')!.value,
-      dct_producto: this.formDetalle.get('dct_producto')!.value,
-      dct_cantidad: this.formDetalle.get('dct_cantidad')!.value,
-      dct_precio_descuento: this.formDetalle.get('dct_precio_descuento')!.value,
-      dct_total: this.formDetalle.get('dct_total')!.value,
+      dct_cotizacion: this.formPedido.get('cot_numero')!.value,
+      dct_numero_detalle: this.formPedido.get('formDetalle.dct_numero_detalle')!.value,
+      dct_producto: this.formPedido.get('formDetalle.dct_producto')!.value,
+      dct_cantidad: this.formPedido.get('formDetalle.dct_cantidad')!.value,
+      dct_precio_descuento: this.formPedido.get('formDetalle.dct_precio_descuento')!.value,
+      dct_total: 0,
     }
 
     console.log(detalle_pedido);
+    this.calcularTotales(detalle_pedido);
     this.detalle_pedidos.push(detalle_pedido);
-    this.formDetalle.reset();
+    this.formPedido.get('formDetalle')!.reset();
   }
 
   editar(){
     const pedido: Pedido = {
       cot_empresa: this.userSesion.empresa,
-      cot_numero: this.form.get('cot_numero')!.value,
-      cot_pedido: this.form.get('cot_pedido')!.value,
-      cot_vendedor: this.form.get('cot_vendedor')!.value,
-      cot_bodega: this.form.get('cot_bodega')!.value,
-      cot_cliente: this.form.get('cot_cliente')!.value,
-      cot_tipo_documento: this.form.get('cot_tipo_documento')!.value,
-      cot_total: this.form.get('cot_total')!.value,
+      cot_numero: this.formPedido.get('cot_numero')!.value,
+      cot_pedido: this.formPedido.get('cot_pedido')!.value,
+      cot_vendedor: this.formPedido.get('cot_vendedor')!.value,
+      cot_bodega: this.formPedido.get('cot_bodega')!.value,
+      cot_cliente: this.formPedido.get('cot_cliente')!.value,
+      cot_tipo_documento: this.formPedido.get('cot_tipo_documento')!.value,
+      cot_total: this.total_pedido,
       cot_anulada: false,
-      detalles: []
+      detalles: this.detalle_pedidos
     }
 
     this.pedidoService.actualizarPedido(this.cot_empresa ,this.cot_empresa ,this.cot_empresa ,pedido).subscribe(data => {
       this.toastr.info('Registro Modificado', 'Pedido Modificado Exitosamente');
       this.pedidoService.obtenerPedidos();
-      this.form.reset();
-      this.formDetalle.reset();
+      this.formPedido.reset();
       this.cot_empresa = ""; this.cot_empresa = "" ;this.cot_empresa = "";
     });
   }
+
+  //Se valida por separado porque posee un formulario Interno de Detalle
+  validarPedido(){
+    let valido = false;
+    if(
+      this.formPedido.get('cot_numero')!.valid &&
+      this.formPedido.get('cot_pedido')!.valid &&
+      this.formPedido.get('cot_bodega')!.valid &&
+      this.formPedido.get('cot_vendedor')!.valid &&
+      this.formPedido.get('cot_cliente')!.valid &&
+      this.formPedido.get('cot_tipo_documento')!.valid &&
+      this.detalle_pedidos.length > 0
+    ){
+      valido = true;
+    }
+    return valido;
+  }
+
+  //Operaciones aritmeticas
+  calcularTotales(detalle: DetallePedido){
+    detalle.dct_total = detalle.dct_cantidad*detalle.dct_precio_descuento;
+    this.total_pedido = this.total_pedido + detalle.dct_total;
+ }
 
 }

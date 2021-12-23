ec8a6103ef766c5521fd30f6503faa94c149a11b
ManuelAlvarado11 <av16002@ues.edu.sv>
2021-12-23 12:09:19 -0600

    Eliminacion de archivos innecesarios de numeracion

--- src/app/models/cnfCorrelativoPedidos.ts
+++
@@ -1,6 +0,0 @@
-export class CnfCorrelativoPedidos{
-    cnf_empresa!:string;
-    cnf_bodega!: string;
-    cnf_correlativos!: number;
-    cnf_ceros_correlativos!: string;
-}
--- src/app/models/numeracionFacturacion.ts
+++
@@ -1,5 +0,0 @@
-export class numeracionFacturacion{
-    num_empresa!:string;
-    num_tabla!: string;
-    num_numero!: number;
-}
